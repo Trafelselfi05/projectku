91eda0ed5d00abb9158ba5d70c5a80bee0c40efc
Trafel Selfi <selfitrafel@gmail.com>
2023-06-24 07:24:59 +0700

    edit body

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar/>
+    <div class="container">
+    <HeroView/>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '@/components/Navbar.vue'
+import HeroView from '@/components/Hero.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Navbar,
+    HeroView
   }
 }
 </script>
