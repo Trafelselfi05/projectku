52d3eaa4ffee35b4e3437eb86bfe1fd595e0a614
Trafel Selfi <selfitrafel@gmail.com>
2023-06-24 19:51:56 +0700

    edit body

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,7 +15,7 @@
       </div>
 
       <div class="row mb-3">
-        <div class="col-md 3 mt-4">
+        <div class="col-md 3 mt-4" v-for="product in products" :key="product.id">
           <CardProduct />
         </div>
       </div>
@@ -28,12 +28,36 @@
 import Navbar from "@/components/Navbar.vue";
 import HeroView from "@/components/Hero.vue";
 import CardProduct from "@/components/CardProduct.vue";
+import axios from "axios";
 export default {
+
   name: "HomeView",
   components: {
     Navbar,
     HeroView,
     CardProduct,
   },
+  data(){
+    return {
+      products: []
+    }
+  }, 
+  methods: {
+    setProduct(data) {
+      this.products.push=data
+    }
+  }, 
+  mounted() {
+  axios
+  .get('http://localhost:3000/best-productsasdas')
+  .then(function (response) {
+    // handle success
+    this.setProduct(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log("Gagal : ",error);
+  })
+}
 };
 </script>
