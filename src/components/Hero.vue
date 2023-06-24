9f11db896e1adf33ebb392a463c0a9f701c1153c
Trafel Selfi <selfitrafel@gmail.com>
2023-06-24 09:19:03 +0700

    edit body

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,20 +1,26 @@
 <template>
-  <div class="row">
+  <div class="row mt-4">
     <div class="col">
-        <h2><strong>Delicius Foods Menu,</strong><br>In Your Gadget</h2>
+      <div class="d-flex h-100">
+        <div class="justify-content-center align-self-center">
+          <h2>
+            <strong>Tersedia Banyak makanan Lezat,</strong><br />Di gadget kamu</h2>
+            <p>Ayo Buruan Pesan dan Pilih Makanan Favorit Kamu</p>
+            <button class="btn btn-lg btn-success"><b-icon-arrow-right></b-icon-arrow-right>Pesan</button>
+        </div>
+      </div>
     </div>
     <div class="col">
-
+      <img src="../assets/images/hero.svg" width="100%" />
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name : 'HeroView',
-}
+  name: "HeroView",
+};
 </script>
 
 <style>
-
 </style>
