e198ba1f2d89199ee5a31ebad965ad4e9b861b46
Trafel Selfi <selfitrafel@gmail.com>
2023-06-25 15:01:32 +0700

    edit body

--- src/views/FoodDetail.vue
+++ src/views/FoodDetail.vue
@@ -1,64 +1,84 @@
 <template>
-  <div class="food-detail">
-    <Navbar />
-    <div class="container">
+    <div class="food-detail">
+      <Navbar />
+      <div class="container">
         <!--breadcrumbs -->
-      <div class="row">
-        <div class="col-mt-5">
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-             <li class="breadcrumb-item" aria-current="page">
-              <router-link to="/" class="text-dark">Home</router-link>
-              </li>
-              <li class="breadcrumb-item" aria-current="page">
-              <router-link to="/foods" class="text-dark">Home</router-link>
-            </li>
-            <li class="breadcrumb-item active" aria-current="page">Food Order</li>
-            </ol>
-          </nav>
+        <div class="row">
+          <div class="col-mt-5">
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item" aria-current="page">
+                  <router-link to="/" class="text-dark">Home</router-link>
+                </li>
+                <li class="breadcrumb-item" aria-current="page">
+                  <router-link to="/foods" class="text-dark">Food</router-link>
+                </li>
+                <li class="breadcrumb-item active" aria-current="page">
+                  Food Order
+                </li>
+              </ol>
+            </nav>
+          </div>
+        </div>
+  
+        <div class="row">
+          <div class="col-md-6">
+            <img
+              :src="'../assets/images/' + product.gambar"
+              class="img-fluid"
+              alt=""
+            />
+          </div>
+          <div class="col-md-6">
+            <h2>
+              <strong>{{ product.nama }}</strong>
+            </h2>
+            <hr>
+            <h4>Harga : <strong>Rp. {{ product.harga }}</strong></h4>
+            <form>
+              <div class="form-group">
+                <label for="jumlah_pesanan">Jumlah Pesan</label>
+                <input type="number" class="form-control" />
+              </div>
+              <div class="form-group">
+                <label for="keterangan">Keterangan</label>
+                <textarea class="form-control" placeholder="Keterangan Seperti : Pedas, Nasi Setengah ..."></textarea>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-
-
-      <div class="row">
-        <div class="col md-6">
-            <!--<img :src="../assets/images"+>-->
-        </div>
-      </div>
-      <div class="col md-6">
-        <h2>Nama Makanan</h2>
-      </div>
-
     </div>
-  </div>
-</template>
-
-<script>
-import Navbar from "@/components/Navbar.vue";
-import axios from "axios";
-export default {
-  name: "FoodDetail",
-  components: {
-    Navbar,
-  },
-  data(){
-return {
-    product :{}
-    }
-  },
-  methods: {
-    setProduct(data) {
-        this.products = data
-    }
-  },
-  mounted() {
-    axios
-      .get("http://localhost:3000/products/"+this.$route.params.id)
-      .then((response) => this.setProduct(response.data))
-      .catch((error) => console.log(error));
-  },
-};
-</script>
-
-<style>
-</style>
+  </template>
+  
+  <script>
+  import Navbar from "@/components/Navbar.vue";
+  import axios from "axios";
+  
+  export default {
+    name: "FoodDetail",
+    components: {
+      Navbar,
+    },
+    data() {
+      return {
+        product: {},
+      };
+    },
+    methods: {
+      setProduct(data) {
+        this.product = data;
+      },
+    },
+    mounted() {
+      axios
+        .get("http://localhost:3000/products/" + this.$route.params.id)
+        .then((response) => this.setProduct(response.data))
+        .catch((error) => console.log(error));
+    },
+  };
+  </script>
+  
+  <style>
+  </style>
+  
