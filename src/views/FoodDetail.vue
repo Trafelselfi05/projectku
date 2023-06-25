482f90fabe40c1b6d6c85813799b17c93474c616
Trafel Selfi <selfitrafel@gmail.com>
2023-06-25 09:50:10 +0700

    edit body

---
+++ src/views/FoodDetail.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="food-detail">
+        <Navbar />
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  name: "FoodDetail",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style>
+</style>
